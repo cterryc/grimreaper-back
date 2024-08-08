c689d1c2b91042f5a957ff241fb5c1c879f90c95
Your Name <danyel.martel@gmail.com>
2024-08-08 18:55:01 -0500

    add backUp

---
+++ src/models/secondBackUp.models.js
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize'
+import DATA_BASE from '../config/db.js'
+
+const SecondBackUp = DATA_BASE.define('second', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+    unique: true
+  },
+  gearscore: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  class: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  net: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  spent: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  total: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  hours: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  rank: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+})
+
+export default SecondBackUp
