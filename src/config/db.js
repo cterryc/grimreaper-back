759c3db4262cca6e7a52be712c18699e1b7b1364
Terry <danyel.martel@gmail.com>
2024-08-19 04:45:04 -0500

    pg are import manually

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import { Sequelize } from 'sequelize'
+import pg from 'pg'
 dotenv.config()
 const { PORT_DB, USER_DB, PASS_DB, NAME_DB, URL_DB, LOCAL } = process.env
 
@@ -9,7 +10,8 @@ if (LOCAL) {
   DATA_BASE = new Sequelize(
     `postgres://${USER_DB}:${PASS_DB}@${PORT_DB}/${NAME_DB}`,
     {
-      logging: console.log('Local Data Base')
+      logging: console.log('Local Data Base'),
+      dialectModule: pg
     }
   )
 } else {
